f8de2427e42f4fb9a00ef93f1871fda83a7251c3
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-26 16:40:04 -0400

    Whiteboard

---
+++ FizzBuzzWhiteboard/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FizzBuzzWhiteboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzBuzz(6));
+            Console.WriteLine(FizzBuzz(8));
+            Console.WriteLine(FizzBuzz(10));
+            Console.WriteLine(FizzBuzz(13));
+            Console.WriteLine(FizzBuzz(15));
+            Console.WriteLine(FizzBuzz(19));
+            Console.WriteLine(FizzBuzz(20));
+            Console.WriteLine(FizzBuzz(22));
+            Console.WriteLine(FizzBuzz(24));
+            Console.WriteLine(FizzBuzz(28));
+            Console.WriteLine(FizzBuzz(30));
+        }
+
+        public static string FizzBuzz(int num)
+        {
+            string result = "";
+
+            if (num % 3 == 0)
+                result = "fizz";
+
+            if (num % 5 == 0)
+                result += "buzz";
+
+            return result;
+        }
+    }
+}
